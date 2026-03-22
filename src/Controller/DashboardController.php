7465d62688c7396f84064be1bd71d7ca8994fe2f
Vladimir Malits <vladimir.malits@gmail.com>
2026-03-22 21:46:08 +0200

    Add click counter to dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\ShortUrl;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\ClickRepository;
+use App\Repository\ShortUrlRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -14,7 +14,8 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class DashboardController extends AbstractController
 {
     public function __construct(
-        private readonly EntityManagerInterface $em,
+        private readonly ShortUrlRepository $shortUrlRepository,
+        private readonly ClickRepository $clickRepository,
     ) {
     }
 
@@ -22,11 +23,12 @@ final class DashboardController extends AbstractController
     #[IsGranted('ROLE_USER')]
     public function index(): Response
     {
-        $shortUrls = $this->em->getRepository(ShortUrl::class)
-            ->findBy(['user' => $this->getUser()], ['createdAt' => 'DESC']);
+        $shortUrls = $this->shortUrlRepository->findBy(['user' => $this->getUser()], ['createdAt' => 'DESC']);
+        $clickCounts = $this->clickRepository->countGroupedByShortUrl($shortUrls);
 
         return $this->render('dashboard/index.html.twig', [
             'shortUrls' => $shortUrls,
+            'clickCounts' => $clickCounts,
         ]);
     }
 }
--- src/Repository/ClickRepository.php
+++ src/Repository/ClickRepository.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\Click;
+use App\Entity\ShortUrl;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -16,5 +17,34 @@ class ClickRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Click::class);
+    }
+
+    /**
+     * @param array<ShortUrl> $shortUrls
+     *
+     * @return array<int, int>
+     */
+    public function countGroupedByShortUrl(array $shortUrls): array
+    {
+        if (empty($shortUrls)) {
+            return [];
+        }
+
+        $result = $this->getEntityManager()
+            ->createQuery('
+                SELECT IDENTITY(c.shortUrl) AS shortUrlId, COUNT(c.id) AS count
+                FROM App\Entity\Click c
+                WHERE c.shortUrl IN (:urls)
+                GROUP BY c.shortUrl
+            ')
+            ->setParameter('urls', $shortUrls)
+            ->getResult();
+
+        $counts = [];
+        foreach ($result as $row) {
+            $counts[(int) $row['shortUrlId']] = (int) $row['count'];
+        }
+
+        return $counts;
     }
 }
